248e2edbdd84014990245bc7d654aefa01533199
Ariethel <a.renzullo9@studenti.unisa.it>
2023-01-21 12:22:35 +0100

    Provati nuovi algoritmi di regressione e trovato un modo bruttissimo di verificare la linearita dei dati numerici su un grafico

--- pricePredictor.py
+++ pricePredictor.py
@@ -1,26 +1,38 @@
+import os
+
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import BayesianRidge
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import plotly.express as px
 import tkinter as tk
 from tkinter import filedialog
-import os
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression, ElasticNet, BayesianRidge
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        # Cancello i file residui dalle vecchie iterazioni del programma
         self.cancella_file()
+
+        # Inizializzo un po' di variabili che mi servono
         self.filepath = None
         self.variablepath = None
         self.X = None
         self.y = None
         self.model = None
+
+        # Pulisco i dati
         self.clean_data()
+
+        # Addestro il modello
         self.training()
+
+        # Creo la finestra
         self.geometry("200x200")
         self.title("File Browser")
         self.output_label = tk.Label(self, text="")
@@ -29,11 +41,14 @@ class App(tk.Tk):
         self.file_browse_button.pack()
         self.file_browse_button = tk.Button(self, text="Select test variable", command=self.browse_variable)
         self.file_browse_button.pack()
-        self.predict_button = tk.Button(self, text="Predict", command=self.prevision)
+        # Con questo bottone effettuo le predizioni
+        self.predict_button = tk.Button(self, text="Predict", command=self.prediction)
         self.predict_button.pack()
 
 
-    # Semplice funzione per eliminare i file residui tra una prova e l'altra
+
+
+    # Funzione per eliminare i file residui tra una prova e l'altra
     def cancella_file(self):
         if os.path.exists("pred.csv"):
             os.remove("pred.csv")
@@ -46,30 +61,55 @@ class App(tk.Tk):
 
 
 
+    # Funzione per pulire i dati e riempire i campi vuoti
     def clean_data(self):
         # Leggo il file csv
         df = pd.read_csv('used_device_data.csv')
 
         # Stampo alcune info sul dataframe, in modo da verificare i valori nulli ed il tipo di dato per ogni colonna
         print(df.info())
-        px.bar(df, x="device_brand", y="normalized_used_price").show()
 
-        # Isolo la colonna che voglio predire
+        # Creo un grafico con tutte le caratteristiche numeriche per verificare la loro distribuzione
+        scaler = MinMaxScaler()
+        df_columns_to_normalize = df[['screen_size', 'rear_camera_mp', 'front_camera_mp', 'internal_memory', 'ram', 'battery', 'weight', 'days_used', 'normalized_new_price']]
+        df_columns_to_normalize = scaler.fit_transform(df_columns_to_normalize)
+        df[['screen_size', 'rear_camera_mp', 'front_camera_mp', 'internal_memory', 'ram', 'battery', 'weight', 'days_used', 'normalized_new_price']] = df_columns_to_normalize
+        ax1 = df.plot(kind="scatter", x="screen_size", y="normalized_used_price", color="r")
+        df.plot(kind="scatter", x="rear_camera_mp", y="normalized_used_price", color="g", ax=ax1)
+        df.plot(kind="scatter", x="front_camera_mp", y="normalized_used_price", color="b", ax=ax1)
+        df.plot(kind="scatter", x="internal_memory", y="normalized_used_price", color="y", ax=ax1)
+        df.plot(kind="scatter", x="ram", y="normalized_used_price", color="m", ax=ax1)
+        df.plot(kind="scatter", x="battery", y="normalized_used_price", color="c", ax=ax1)
+        df.plot(kind="scatter", x="weight", y="normalized_used_price", color="k", ax=ax1)
+        df.plot(kind="scatter", x="days_used", y="normalized_used_price", color="orange", ax=ax1)
+        df.plot(kind="scatter", x="normalized_new_price", y="normalized_used_price", color="brown", ax=ax1)
+        plt.show()
+
+        # Verifico la correlazione con il test statistico di Pearson e anche da questo vedo che l'andamento e' lineare
+        df.corr().to_csv('correlation.csv')
+
+
+        # Leggo di nuovo il file csv per pulire i dati
+        df = pd.read_csv('used_device_data.csv')
+
+        # Isolo la colonna che voglio predire e la rimuovo dal dataframe
         y = df['normalized_used_price']
         df.drop(['normalized_used_price'], axis=1, inplace=True)
+
 
         # Trasformo le variabili categoriche in numeriche grazie al oneHotEncoding incluso in pandas
         df = pd.get_dummies(df)
         imputer = SimpleImputer(strategy='most_frequent')
-
         # Creo un nuovo dataframe perche' il simple imputer me lo ha trasformato in array numpy
         X = pd.DataFrame(imputer.fit_transform(df))
         self.X = X
         self.y = y
 
 
-    def training(self):
 
+    # Funzione di addestramento del modello
+
+    def training(self):
         # Leggo i dati gia' puliti
         X = self.X
         y = self.y
@@ -80,29 +120,43 @@ class App(tk.Tk):
         # Creo ed addestro il modello
         model = LinearRegression()
 
-        # il bayesian ridge ha un errore maggiore
+        # Altri modelli che ho provato ma che hanno dato risultati peggiori
+        #model = ElasticNet(random_state=0)
         #model = BayesianRidge()
-
-
-        #il decision tree si e' dimostrato peggiore
         #model = DecisionTreeRegressor()
 
         model.fit(X_train, y_train)
+
+        # Faccio dei test per vedere se il training e' andato a buon fine o se sono in underfitting
+        pred = model.predict(X_train)
+        mae = mean_absolute_error(y_train, pred)
+        mse = mean_squared_error(y_train, pred)
+        r2 = r2_score(y_train, pred)
+        print(f"TRAINING RESULTS:\nMAE: {mae}\nMSE: {mse}\nR2: {r2}")
 
         # Creo due file csv per il test
         X_test.to_csv('X_test.csv', index=False)
         y_test.to_csv('y_test.csv', index=False)
         self.model = model
 
+
+
+    # Scelta del csv contenente la variabile dipendente di test
     def browse_variable(self):
         # Seleziono il file contenente la variabile dipendente del test set
         self.variablepath = filedialog.askopenfilename()
 
+
+
+    # Scelta del csv contenente le variabili indipendenti di test
     def browse_file(self):
         # Seleziono il test set
         self.filepath = filedialog.askopenfilename()
 
-    def prevision(self):
+
+
+    # Funzione per effettuare le predizioni
+    def prediction(self):
         # Prendo il modello gia' addestrato
         model = self.model
         X_test = pd.read_csv(self.filepath)
@@ -118,7 +172,7 @@ class App(tk.Tk):
         mae = mean_absolute_error(y_test, pred)
         mse = mean_squared_error(y_test, pred)
         r2 = r2_score(y_test, pred)
-        self.output_label.configure(text=f"MAE: {mae} \nMSE: {mse} \nR2: {r2}")
+        self.output_label.configure(text=f"TEST RESULTS:\nMAE: {mae} \nMSE: {mse} \nR2: {r2}")
 
 
 app = App()
